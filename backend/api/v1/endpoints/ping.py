90e3f7918966019b64f1365466360fcda5c744bb
dzherb <zherbin.dima@yandex.ru>
2025-04-15 01:34:04 +0300

    chore: format and fix typing

--- backend/api/v1/endpoints/ping.py
+++ backend/api/v1/endpoints/ping.py
@@ -1,7 +1,9 @@
+import typing
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from dependencies import get_session
@@ -16,4 +18,8 @@ class Ping(BaseModel):
 
 @router.get('')
 async def ping(session: Annotated[AsyncSession, Depends(get_session)]) -> Ping:
-    return Ping(ok=True, database=session.bind.url.database)
+    engine = typing.cast(AsyncEngine, session.bind)
+    return Ping(
+        ok=True,
+        database=engine.url.database,
+    )
--- backend/tests/api/endpoints/test_ping.py
+++ backend/tests/api/endpoints/test_ping.py
@@ -4,4 +4,7 @@ from httpx import AsyncClient
 async def test_ping(client: AsyncClient, migrated_db: str) -> None:
     response = await client.get('/api/v1/ping')
     assert response.status_code == 200
-    assert response.json() == {'ok': True, 'database': migrated_db.split('/')[-1]}
+    assert response.json() == {
+        'ok': True,
+        'database': migrated_db.split('/')[-1],
+    }
