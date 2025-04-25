aff7efec1e336eef2b374eead9ff7123df4049ab
dzherb <zherbin.dima@yandex.ru>
2025-04-25 20:34:26 +0300

    chore: add coverage.sh script for test coverage reporting

---
+++ backend/scripts/coverage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PROJECT_ROOT="$SCRIPT_DIR/../"
+
+uv run coverage run --source="$SCRIPT_DIR/../" -p -m pytest
+uv run coverage combine
+uv run coverage html
+
+python -m webbrowser -t "file:///$PROJECT_ROOT/htmlcov/index.html"
